0c034e19e8e52c8c123458ddd8ff38774f84ea30
chansol4517 <ckssol4517@gmail.com>
2024-09-23 10:11:47 +0900

    현재 시간값을 활용해 am,pm출력 삼항연산자로 분기처리

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //미션1 - new Date()를 활용해서 시간,분,초를 각각 span안에 출력
 //미션2 - 현재 시간이 13시를 넘기면 em안쪽의 'am'을 'pm'로 출력
+
 const title = document.querySelector("h1");
 const [em, spanHr, spanMin, spanSec] = title.children;
 
