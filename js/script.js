e2fc899e203d37908c8ab1f4c66ef42a25b01b00
chansol4517 <ckssol4517@gmail.com>
2024-09-23 10:10:42 +0900

    현재 시간값을 활용해 am, pm출력

--- js/script.js
+++ js/script.js
@@ -1,56 +1,15 @@
-console.log("test");
-
-/*
-new Date() : 시간관련 인스턴스 객체를 생성해주는 생성자
-시간 관련 인스턴스 객체 : 다양한 시간관련 메서드들 호출 가능
-*/
-
-const month = [
-  "JAN",
-  "FEB",
-  "MAR",
-  "APR",
-  "MAY",
-  "JUN",
-  "JUL",
-  "AUG",
-  "SEP",
-  "OCT",
-  "NOV",
-  "DEC",
-];
-
-const days = ["SUN", "MON", "TUE", "WEN", "THUR", "FRI", "SAT"];
+//미션1 - new Date()를 활용해서 시간,분,초를 각각 span안에 출력
+//미션2 - 현재 시간이 13시를 넘기면 em안쪽의 'am'을 'pm'로 출력
+const title = document.querySelector("h1");
+const [em, spanHr, spanMin, spanSec] = title.children;
 
 const now = new Date();
+const hr = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+let apm = hr < 13 ? "am" : "pm";
 
-console.log(now);
-
-//연도 출력
-console.log(now.getFullYear());
-
-//월 출력(순서값 반환)
-console.log(month[now.getMonth()]);
-
-//일
-console.log(now.getDate());
-
-//요일(순서값 반환)
-console.log(days[now.getDay()]);
-
-//시간
-console.log(days[now.getHours]);
-
-//분
-console.log(now.getMinutes());
-
-//초
-console.log(now.getSeconds());
-
-//우리나라 시간대를 전세계 표준시로 변환해서 반환
-// const gmtNow = now.toGMTString();
-
-//반환값은 똑같지만 구조만 바뀜
-const gmtNow = now.toUTCString();
-
-console.log(gmtNow);
+em.innerText = apm;
+spanHr.innerText = hr;
+spanMin.innerText = min;
+spanSec.innerText = sec;
